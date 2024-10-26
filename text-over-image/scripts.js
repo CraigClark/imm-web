6dcfc77eeefcc1c3cffa98c21849b55b3b8d2062
Craig CLark <clark.craig@gmail.com>
2024-10-26 08:17:46 -0400

    add all toggles for space demo

--- text-over-image/scripts.js
+++ text-over-image/scripts.js
@@ -1,18 +1,18 @@
 document.getElementById("background").addEventListener("click", function () {
-  // Get the current value of --container-bg
+  // Get the current value of --container-bg-space
   const currentBg = getComputedStyle(document.documentElement)
-    .getPropertyValue("--container-bg")
+    .getPropertyValue("--container-bg-space")
     .trim();
 
   // Toggle between the two values
   if (currentBg === "rgba(21, 10, 18, 0.47)") {
     document.documentElement.style.setProperty(
-      "--container-bg",
+      "--container-bg-space",
       "rgba(21, 10, 18, 0)"
     ); // Reset to transparent
   } else {
     document.documentElement.style.setProperty(
-      "--container-bg",
+      "--container-bg-space",
       "rgba(21, 10, 18, 0.47)"
     ); // Set to 47% transparency
   }
@@ -28,6 +28,21 @@ document.getElementById("color").addEventListener("click", function () {
   document.documentElement.style.setProperty(
     "--title-color-space",
     currentColor === "#ceb8a0" ? "#FCF6C2" : "#ceb8a0"
+  );
+});
+
+document.getElementById("shadow").addEventListener("click", function () {
+  // Get the current value of --text-shadow-space
+  const currentShadow = getComputedStyle(document.documentElement)
+    .getPropertyValue("--text-shadow-space")
+    .trim();
+
+  // Toggle between the default value and the specified shadow
+  document.documentElement.style.setProperty(
+    "--text-shadow-space",
+    currentShadow === "6px 6px 13px rgba(0, 0, 0, 0.89)"
+      ? "none"
+      : "6px 6px 13px rgba(0, 0, 0, 0.89)"
   );
 });
 
